930fac1f40dc8dada9853b814d3fcb3f787b2c8e
tasos14 <tasos14tasos@gmail.com>
2016-12-17 15:36:32 +0200

    es5 to es6+ syntax

--- src/components/GridSizes.js
+++ src/components/GridSizes.js
@@ -1,14 +1,20 @@
-var React = require('react');
+import React from 'react';
 
-var GridSizes = React.createClass({
-  handleClick: function(e){
+export default class GridSizes extends React.Component {
+  constructor(props) {
+    super(props);
+    // Operations usually carried out in componentWillMount go here
+    // this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick = (e) => {
     var grid = parseInt(e.target.id);
     this.props.onClick(grid);
-  },
+  }
 
-  render: function(){
-    var elements = [];
-    for(var i=4; i<9; i++){
+  render() {
+    let elements = [];
+    for(let i=4; i<9; i++){
       elements.push(<li key={"grid"+i}><a onClick={this.handleClick} id={i}>{i+"x"+i}</a></li>);
     }
 
@@ -18,6 +24,5 @@ var GridSizes = React.createClass({
       </ul>
     );
   }
-});
 
-module.exports = GridSizes;
+}
