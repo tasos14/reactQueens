45b49a08fdf98fbcd55a7dba7af3dec783216f1d
tasos14 <tasos14tasos@gmail.com>
2017-01-10 15:43:22 +0200

    added handler for report.html

--- src/server.js
+++ src/server.js
@@ -20,8 +20,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 
-app.get("*", function(req, res) {
+app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "static/index.html"));
+});
+
+app.get("/report", function(req, res) {
+  res.sendFile(path.join(__dirname, "static/report.html"));
 });
 
 app.post('/', (req, res) => {
