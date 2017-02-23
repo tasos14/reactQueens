28f4d74a4da4b43f5ca330619adad722c2b6d3f9
tasos14 <tasos14tasos@gmail.com>
2017-02-23 12:22:27 +0200

    converted GridSizes to functional stateless component

--- src/components/GridSizes.js
+++ src/components/GridSizes.js
@@ -1,28 +1,48 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 
-export default class GridSizes extends React.Component {
-  constructor(props) {
-    super(props);
-    // Operations usually carried out in componentWillMount go here
-    // this.handleClick = this.handleClick.bind(this);
+const GridSizes = ({ onClick }) => {
+  let elements = [];
+
+  for(let i=4; i<9; i++){
+    elements.push(<li key={"grid"+i}><a onClick={() => onClick(i)} id={i}>{i+"x"+i}</a></li>);
   }
 
-  handleClick = (e) => {
-    var grid = parseInt(e.target.id);
-    this.props.onClick(grid);
-  }
+  return (
+    <ul className="dropdown-menu">
+      {elements}
+    </ul>
+  );
+};
 
-  render() {
-    let elements = [];
-    for(let i=4; i<9; i++){
-      elements.push(<li key={"grid"+i}><a onClick={this.handleClick} id={i}>{i+"x"+i}</a></li>);
-    }
+GridSizes.propTypes = {
+  onClick: PropTypes.func.isRequired
+};
 
-    return (
-      <ul className="dropdown-menu">
-        {elements}
-      </ul>
-    );
-  }
+export default GridSizes;
 
-}
+// export default class GridSizes extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     // Operations usually carried out in componentWillMount go here
+//     // this.handleClick = this.handleClick.bind(this);
+//   }
+//
+//   handleClick = (e) => {
+//     var grid = parseInt(e.target.id);
+//     this.props.onClick(grid);
+//   }
+//
+//   render() {
+//     let elements = [];
+//     for(let i=4; i<9; i++){
+//       elements.push(<li key={"grid"+i}><a onClick={this.handleClick} id={i}>{i+"x"+i}</a></li>);
+//     }
+//
+//     return (
+//       <ul className="dropdown-menu">
+//         {elements}
+//       </ul>
+//     );
+//   }
+//
+// }
--- src/static/js/bundle.js
+++ src/static/js/bundle.js
@@ -3486,63 +3486,27 @@
             default: e
         };
     }
-    function o(e, t) {
-        if (!(e instanceof t)) throw new TypeError("Cannot call a class as a function");
-    }
-    function i(e, t) {
-        if (!e) throw new ReferenceError("this hasn't been initialised - super() hasn't been called");
-        return !t || "object" != typeof t && "function" != typeof t ? e : t;
-    }
-    function a(e, t) {
-        if ("function" != typeof t && null !== t) throw new TypeError("Super expression must either be null or a function, not " + typeof t);
-        e.prototype = Object.create(t && t.prototype, {
-            constructor: {
-                value: e,
-                enumerable: !1,
-                writable: !0,
-                configurable: !0
-            }
-        }), t && (Object.setPrototypeOf ? Object.setPrototypeOf(e, t) : e.__proto__ = t);
-    }
     Object.defineProperty(t, "__esModule", {
         value: !0
     });
-    var u = function() {
-        function e(e, t) {
-            for (var n = 0; n < t.length; n++) {
-                var r = t[n];
-                r.enumerable = r.enumerable || !1, r.configurable = !0, "value" in r && (r.writable = !0), 
-                Object.defineProperty(e, r.key, r);
-            }
-        }
-        return function(t, n, r) {
-            return n && e(t.prototype, n), r && e(t, r), t;
-        };
-    }(), s = n(15), c = r(s), l = function(e) {
-        function t(e) {
-            o(this, t);
-            var n = i(this, (t.__proto__ || Object.getPrototypeOf(t)).call(this, e));
-            return n.handleClick = function(e) {
-                var t = parseInt(e.target.id);
-                n.props.onClick(t);
-            }, n;
-        }
-        return a(t, e), u(t, [ {
-            key: "render",
-            value: function() {
-                for (var e = [], t = 4; t < 9; t++) e.push(c.default.createElement("li", {
-                    key: "grid" + t
-                }, c.default.createElement("a", {
-                    onClick: this.handleClick,
-                    id: t
-                }, t + "x" + t)));
-                return c.default.createElement("ul", {
-                    className: "dropdown-menu"
-                }, e);
-            }
-        } ]), t;
-    }(c.default.Component);
-    t.default = l;
+    var o = n(15), i = r(o), a = function(e) {
+        for (var t = e.onClick, n = [], r = function(e) {
+            n.push(i.default.createElement("li", {
+                key: "grid" + e
+            }, i.default.createElement("a", {
+                onClick: function() {
+                    return t(e);
+                },
+                id: e
+            }, e + "x" + e)));
+        }, o = 4; o < 9; o++) r(o);
+        return i.default.createElement("ul", {
+            className: "dropdown-menu"
+        }, n);
+    };
+    a.propTypes = {
+        onClick: o.PropTypes.func.isRequired
+    }, t.default = a;
 }, function(e, t, n) {
     "use strict";
     function r(e) {
