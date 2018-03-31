151d4697dae6195a2d563522649441f8bd5d14bc
Tasos <atheodoridis@trigonongroup.com>
2018-03-31 20:13:40 +0300

    configure eslint

--- webpack.config.js
+++ webpack.config.js
@@ -3,30 +3,35 @@ const webpack = require('webpack');
 module.exports = {
   entry: [
     'react-hot-loader/patch',
-    './src/app-client.js'
+    './src/app-client.js',
   ],
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: ['babel-loader']
-      }
-    ]
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader', 'eslint-loader'],
+      },
+    ],
   },
   output: {
     path: __dirname + 'src' + 'static' + 'js',
-    publicPath: "/js/",
-    filename: 'bundle.js'
+    publicPath: '/js/',
+    filename: 'bundle.js',
   },
   plugins: [
-    new webpack.HotModuleReplacementPlugin()
+    new webpack.HotModuleReplacementPlugin(),
   ],
   devServer: {
-    contentBase: "src/static/",
+    contentBase: 'src/static/',
     hot: true,
     historyApiFallback: {
-      index: '/index-static.html'
-    }
-  }
+      index: '/index-static.html',
+    },
+  },
 };
