088df9765ee4333364683fb44a83e6f1d8838890
Tasos <atheodoridis@trigonongroup.com>
2020-04-23 22:10:23 +0300

    inline-source-map

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,6 +1,6 @@
 require('@babel/polyfill');
 const webpack = require('webpack');
-const path = require('path');
+const path = require('path'); // eslint-disable-line
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const dotenv = require('dotenv');
 
@@ -17,6 +17,7 @@ module.exports = () => {
     resolve: {
       modules: [path.resolve(process.cwd(), 'src'), path.resolve(process.cwd(), 'node_modules')],
     },
+    devtool: 'inline-source-map',
     module: {
       rules: [
         {
@@ -38,7 +39,7 @@ module.exports = () => {
     },
     plugins: [
       new CleanWebpackPlugin({
-        cleanOnceBeforeBuildPatterns: [path.join(__dirname, 'src', 'static', 'js', 'bundle.js')]
+        cleanOnceBeforeBuildPatterns: [path.join(__dirname, 'src', 'static', 'js', 'bundle.js')],
       }),
       new webpack.HotModuleReplacementPlugin(),
       new webpack.DefinePlugin(envKeys),
